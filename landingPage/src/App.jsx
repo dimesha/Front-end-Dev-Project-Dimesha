d05a5d9470edc8efe988c2c6975072965511266c
dimesha <dimesha99kanchani@gmail.com>
2024-04-10 02:35:17 +0530

    add component and create router path

--- landingPage/src/App.jsx
+++ landingPage/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../src/pages/Home';
+import Header from '../src/pages/Header';
+import Footer from '../src/pages/Footer';
+import Herosection from '../src/pages/HeroSection';
+import CTASection from './pages/CSection';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    {/* <Header/> */}
+    <Herosection/>
+    {/* <CTASection/> */}
+
+      {/* <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/hero" element={<Herosection />}/>
+      </Routes> */}
+      <Footer/>
+    </BrowserRouter>
   )
 }
-
-export default App
